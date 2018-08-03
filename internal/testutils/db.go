c10b4a39fb5c7acca2f41bc60c1378e04015f11d
Tim Vaillancourt <tim@timvaillancourt.com>
2018-08-03 15:58:08 +0200

    Update testutils/db.go for sharding

--- internal/testutils/db.go
+++ internal/testutils/db.go
@@ -8,45 +8,51 @@ import (
 )
 
 const (
-	envMongoDBReplsetName    = "TEST_MONGODB_RS"
-	envMongoDBPrimaryPort    = "TEST_MONGODB_PRIMARY_PORT"
-	envMongoDBSecondary1Port = "TEST_MONGODB_SECONDARY1_PORT"
-	envMongoDBSecondary2Port = "TEST_MONGODB_SECONDARY2_PORT"
-	envMongoDBUser           = "TEST_MONGODB_USERNAME"
-	envMongoDBPassword       = "TEST_MONGODB_PASSWORD"
+	envMongoDBReplsetName          = "TEST_MONGODB_RS"
+	envMongoDBPrimaryPort          = "TEST_MONGODB_PRIMARY_PORT"
+	envMongoDBSecondary1Port       = "TEST_MONGODB_SECONDARY1_PORT"
+	envMongoDBSecondary2Port       = "TEST_MONGODB_SECONDARY2_PORT"
+	envMongoDBConfigsvrReplsetName = "TEST_MONGODB_CONFIGSVR_RS"
+	envMongoDBConfigsvr1Port       = "TEST_MONGODB_CONFIGSVR1_PORT"
+	envMongoDBMongosPort           = "TEST_MONGODB_MONGOS_PORT"
+	envMongoDBUser                 = "TEST_MONGODB_USERNAME"
+	envMongoDBPassword             = "TEST_MONGODB_PASSWORD"
 )
 
 var (
-	MongodbHost           = "127.0.0.1"
-	MongodbReplsetName    = os.Getenv(envMongoDBReplsetName)
-	MongodbPrimaryPort    = os.Getenv(envMongoDBPrimaryPort)
-	MongodbSecondary1Port = os.Getenv(envMongoDBSecondary1Port)
-	MongodbSecondary2Port = os.Getenv(envMongoDBSecondary2Port)
-	MongodbUser           = os.Getenv(envMongoDBUser)
-	MongodbPassword       = os.Getenv(envMongoDBPassword)
-	MongodbTimeout        = time.Duration(10) * time.Second
-	defaultAddr           = []string{MongodbHost + ":17001"}
+	MongoDBHost                 = "127.0.0.1"
+	MongoDBReplsetName          = os.Getenv(envMongoDBReplsetName)
+	MongoDBPrimaryPort          = os.Getenv(envMongoDBPrimaryPort)
+	MongoDBSecondary1Port       = os.Getenv(envMongoDBSecondary1Port)
+	MongoDBSecondary2Port       = os.Getenv(envMongoDBSecondary2Port)
+	MongoDBConfigsvrReplsetName = os.Getenv(envMongoDBConfigsvrReplsetName)
+	MongoDBConfigsvr1Port       = os.Getenv(envMongoDBConfigsvr1Port)
+	MongoDBMongosPort           = os.Getenv(envMongoDBMongosPort)
+	MongoDBUser                 = os.Getenv(envMongoDBUser)
+	MongoDBPassword             = os.Getenv(envMongoDBPassword)
+	MongoDBTimeout              = time.Duration(10) * time.Second
+	defaultAddr                 = []string{MongoDBHost + ":17001"}
 )
 
 func dialInfo(addrs []string) *mgo.DialInfo {
 	di := &mgo.DialInfo{
 		Addrs:   addrs,
-		Timeout: MongodbTimeout,
+		Timeout: MongoDBTimeout,
 	}
-	if MongodbReplsetName != "" {
-		di.ReplicaSetName = MongodbReplsetName
+	if MongoDBReplsetName != "" {
+		di.ReplicaSetName = MongoDBReplsetName
 	}
-	if MongodbUser != "" && MongodbPassword != "" {
-		di.Username = MongodbUser
-		di.Password = MongodbPassword
+	if MongoDBUser != "" && MongoDBPassword != "" {
+		di.Username = MongoDBUser
+		di.Password = MongoDBPassword
 	}
 	return di
 }
 
 func PrimaryDialInfo() *mgo.DialInfo {
 	addrs := defaultAddr
-	if MongodbPrimaryPort != "" {
-		addrs = []string{MongodbHost + ":" + MongodbPrimaryPort}
+	if MongoDBPrimaryPort != "" {
+		addrs = []string{MongoDBHost + ":" + MongoDBPrimaryPort}
 	}
 	di := dialInfo(addrs)
 	di.Direct = true
@@ -55,15 +61,34 @@ func PrimaryDialInfo() *mgo.DialInfo {
 
 func ReplsetDialInfo() *mgo.DialInfo {
 	var di *mgo.DialInfo
-	if MongodbSecondary1Port != "" && MongodbSecondary2Port != "" {
+	if MongoDBSecondary1Port != "" && MongoDBSecondary2Port != "" {
 		di = PrimaryDialInfo()
 	} else {
 		di = dialInfo([]string{
-			MongodbHost + ":" + MongodbPrimaryPort,
-			MongodbHost + ":" + MongodbSecondary1Port,
-			MongodbHost + ":" + MongodbSecondary2Port,
+			MongoDBHost + ":" + MongoDBPrimaryPort,
+			MongoDBHost + ":" + MongoDBSecondary1Port,
+			MongoDBHost + ":" + MongoDBSecondary2Port,
 		})
 	}
+	di.Direct = false
+	return di
+}
+
+func ConfigsvrReplsetDialInfo() *mgo.DialInfo {
+	if MongoDBConfigsvrReplsetName == "" || MongoDBConfigsvr1Port == "" {
+		return &mgo.DialInfo{}
+	}
+	di := dialInfo([]string{MongoDBHost + ":" + MongoDBConfigsvr1Port})
+	di.ReplicaSetName = MongoDBConfigsvrReplsetName
+	di.Direct = false
+	return di
+}
+
+func MongosDialInfo() *mgo.DialInfo {
+	if MongoDBMongosPort == "" {
+		return &mgo.DialInfo{}
+	}
+	di := dialInfo([]string{MongoDBHost + ":" + MongoDBMongosPort})
 	di.Direct = false
 	return di
 }
