bdb2ef0b569c444319fbbe7313d77a067e08faf8
Tim Vaillancourt <tim@timvaillancourt.com>
2018-08-06 14:58:26 +0200

    Fix broken tests from adding internal/cluster (#38)
    * add configsvr shard getter
    
    * Test isXXXX funcs with db conn
    
    * Test isXXXX funcs with db conn #2

--- internal/cluster/cluster.go
+++ internal/cluster/cluster.go
@@ -1,45 +1,19 @@
 package cluster
 
 import (
-	"strings"
-
-	"github.com/globalsign/mgo"
-	"github.com/globalsign/mgo/bson"
 	"github.com/percona/mongodb-backup/mdbstructs"
 )
 
-type Shard struct {
-	config  *mdbstructs.ListShardsShard
-	replset *Replset
+type Cluster struct {
+	shards []*Shard
 }
 
-func parseShardURI(uri string) (string, []string) {
-	s := strings.Split(uri, "/")
-	if len(s) == 2 {
-		return s[0], strings.Split(s[1], ",")
+func New(shards []*mdbstructs.Shard) *Cluster {
+	c := &Cluster{
+		shards: make([]*Shard, 0),
 	}
-	return "", []string{}
-}
-
-func NewShard(shard *mdbstructs.ListShardsShard) *Shard {
-	replset, addrs := parseShardURI(shard.Host)
-	return &Shard{
-		config: shard,
-		replset: &Replset{
-			name:  replset,
-			addrs: addrs,
-		},
+	for _, shard := range shards {
+		c.shards = append(c.shards, NewShard(shard))
 	}
-}
-
-// Return the shards within a sharded cluster using the MongoDB 'listShards'
-// server command. This command will only succeed on a mongos or config
-// server.
-//
-// https://docs.mongodb.com/manual/reference/command/listShards/
-//
-func GetListShards(session *mgo.Session) (*mdbstructs.ListShards, error) {
-	listShards := mdbstructs.ListShards{}
-	err := session.Run(bson.D{{"listShards", "1"}}, &listShards)
-	return &listShards, err
+	return c
 }
---
+++ internal/cluster/shard_test.go
@@ -0,0 +1,81 @@
+package cluster
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/globalsign/mgo"
+	"github.com/percona/mongodb-backup/internal/testutils"
+	"github.com/percona/mongodb-backup/mdbstructs"
+)
+
+func TestParseShardURI(t *testing.T) {
+	rs, hosts := parseShardURI("rs/127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019")
+	if rs == "" {
+		t.Fatal("Got empty replset name from .parseShardURI()")
+	} else if len(hosts) != 3 {
+		t.Fatalf("Expected %d hosts but got %d from .parseShardURI()", 3, len(hosts))
+	} else if rs != "rs" {
+		t.Fatalf("Expected replset name %s but got %s from .parseShardURI()", "rs", rs)
+	} else if !reflect.DeepEqual(hosts, []string{"127.0.0.1:27017", "127.0.0.1:27018", "127.0.0.1:27019"}) {
+		t.Fatal("Unexpected hosts list")
+	}
+
+	// too many '/'
+	rs, hosts = parseShardURI("rs///")
+	if rs != "" || len(hosts) > 0 {
+		t.Fatal("Expected empty results from .parseShardURI()")
+	}
+
+	// missing replset
+	rs, hosts = parseShardURI("127.0.0.1:27017")
+	if rs != "" || len(hosts) > 0 {
+		t.Fatal("Expected empty results from .parseShardURI()")
+	}
+}
+
+func TestNewShard(t *testing.T) {
+	shard := NewShard(&mdbstructs.Shard{
+		Id:   "shard1",
+		Host: "rs/127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019",
+	})
+	if shard.replset.name != "rs" {
+		t.Fatalf("Expected 'replset.name' to equal %v but got %v", "rs", shard.replset.name)
+	} else if len(shard.replset.addrs) != 3 {
+		t.Fatalf("Expected 'replset.addrs' to contain %d addresses but got %d", 3, len(shard.replset.addrs))
+	}
+}
+
+func TestGetListShards(t *testing.T) {
+	session, err := mgo.DialWithInfo(testutils.MongosDialInfo())
+	if err != nil {
+		t.Fatalf("Failed to connect to mongos: %v", err.Error())
+	}
+	defer session.Close()
+
+	listShards, err := GetListShards(session)
+	if err != nil {
+		t.Fatalf("Failed to run 'listShards' command: %v", err.Error())
+	} else if listShards.Ok != 1 {
+		t.Fatal("Got non-ok response code from 'listShards' command")
+	} else if len(listShards.Shards) < 1 {
+		t.Fatal("Got zero shards from .GetListShards()")
+	}
+}
+
+func TestGetConfigsvrShards(t *testing.T) {
+	session, err := mgo.DialWithInfo(testutils.ConfigsvrReplsetDialInfo())
+	if err != nil {
+		t.Fatalf("Failed to connect to the configsvr replset: %v", err.Error())
+	}
+	defer session.Close()
+
+	cnfsvrShards, err := GetConfigsvrShards(session)
+	if err != nil {
+		t.Fatalf("Failed to run .GetConfigsvrShards(): %v", err.Error())
+	} else if len(cnfsvrShards) != 1 {
+		t.Fatal("Got empty list of shards, should contain 1 shard")
+	} else if cnfsvrShards[0].Id != testutils.MongoDBReplsetName {
+		t.Fatalf("Got unexpected shard data, expected 1 shard with name %s", testutils.MongoDBReplsetName)
+	}
+}
