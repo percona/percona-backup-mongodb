c8814979231c73d70bc0ae9435ea93b0f0c02a7f
Carlos Salguero <carlos.salguero@percona.com>
2018-08-05 23:03:09 -0300

    Tests for grpc

--- mocks/generate.sh
+++ mocks/generate.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "Generating mocks for Messages_MessagesChatClient"
 mockgen github.com/percona/mongodb-backup/grpc/messages  Messages_MessagesChatClient > mock_messages/messages_chat_client.go
-mockgen github.com/percona/mongodb-backup/grpc/messages  Messages_MessagesChatClient > mock_messages/messages_chat_client.go
+
+echo "Generating mocks for MessagesClient"
 mockgen github.com/percona/mongodb-backup/grpc/messages  MessagesClient > mock_messages/messages_client.go
+
+echo "Generating mocks for MessagesServer"
 mockgen github.com/percona/mongodb-backup/grpc/messages  MessagesServer > mock_messages/messages_server.go
+
+echo "Generating mocks for Messages_MessagesChatServer"
 mockgen github.com/percona/mongodb-backup/grpc/messages  Messages_MessagesChatServer > mock_messages/messages_chat_server.go
