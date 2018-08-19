f45f119d9f55def9cbbc3f46229c99ce86a33bbe
Tim Vaillancourt <tim@timvaillancourt.com>
2018-08-20 01:08:26 +0200

    internal/hotbackup v0 (#57)
    * Add hotbackup support
    
    * Add hotbackup support #2
    
    * Add hotbackup support #3
    
    * Add hotbackup support #4
    
    * Test hotbackup
    
    * More updates for hot backup
    
    * Fix tests

---
+++ internal/hotbackup/hotbackup.go
@@ -0,0 +1,93 @@
+package hotbackup
+
+import (
+	"errors"
+	"os"
+	"strings"
+
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+)
+
+func isLocalhostSession(session *mgo.Session) (bool, error) {
+	// get system hostname
+	hostname, err := os.Hostname()
+	if err != nil {
+		return false, err
+	}
+
+	// check the server host == os.Hostname
+	status := struct {
+		Host string `bson:"host"`
+	}{}
+	err = session.Run(bson.D{{"serverStatus", "1"}}, &status)
+	if err != nil {
+		return false, err
+	}
+	split := strings.Split(status.Host, ":")
+	if split[0] != hostname {
+		return false, nil
+	}
+
+	// check connection is direct
+	servers := session.LiveServers()
+	if len(servers) != 1 {
+		return false, errors.New("session is not direct")
+	}
+	split = strings.Split(servers[0], ":")
+	for _, match := range []string{"127.0.0.1", "localhost", hostname} {
+		if split[0] == match {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+type HotBackup struct {
+	backupDir string
+	removed   bool
+}
+
+// New creates a Percona Server for MongoDB Hot Backup. The provided
+// MongoDB session must be a direct connection to localhost/127.0.0.1
+//
+// https://www.percona.com/doc/percona-server-for-mongodb/LATEST/hot-backup.html
+//
+func New(session *mgo.Session, backupDir string) (*HotBackup, error) {
+	isLocalhost, err := isLocalhostSession(session)
+	if err != nil {
+		return nil, err
+	} else if !isLocalhost {
+		return nil, errors.New("session must be direct session to localhost or 127.0.0.1")
+	}
+	hb := HotBackup{backupDir: backupDir}
+	err = session.Run(bson.D{{"createBackup", 1}, {"backupDir", hb.backupDir}}, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &hb, nil
+}
+
+// Dir returns the path to the Hot Backup directory
+func (hb *HotBackup) Dir() string {
+	return hb.backupDir
+}
+
+// Remove removes the Hot Backup directory and data
+func (hb *HotBackup) Remove() error {
+	if hb.removed {
+		return nil
+	}
+	err := os.RemoveAll(hb.backupDir)
+	if err != nil {
+		return err
+	}
+	hb.backupDir = ""
+	hb.removed = true
+	return nil
+}
+
+// Close cleans-up and removes the Hot Backup
+func (hb *HotBackup) Close() {
+	_ = hb.Remove()
+}
---
+++ internal/hotbackup/hotbackup_test.go
@@ -0,0 +1,95 @@
+package hotbackup
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/globalsign/mgo"
+	"github.com/percona/mongodb-backup/internal/testutils"
+)
+
+const (
+	testBackupRealPath      = "testdata/backup"
+	testBackupContainerPath = "/data/backup"
+)
+
+func TestHotBackupNew(t *testing.T) {
+	session, err := mgo.DialWithInfo(testutils.PrimaryDialInfo())
+	if err != nil {
+		t.Fatalf("Failed to get primary session: %v", err.Error())
+	}
+	defer session.Close()
+
+	backupName := "primary-" + strconv.Itoa(int(time.Now().Unix()))
+	containerBackupDir := filepath.Join(testBackupContainerPath, backupName)
+	realBackupDir := filepath.Join(testBackupRealPath, backupName)
+
+	// this backup should succeed. use docker volume mount to check
+	// the hot backup dir was created
+	if _, err := os.Stat(realBackupDir); err == nil {
+		t.Fatalf("Backup dir should not exist before backup: %s", realBackupDir)
+	}
+	hb, err := New(session, containerBackupDir)
+	if err != nil {
+		t.Fatalf("Failed to run .New(): %v", err.Error())
+	} else if hb.removed || hb.backupDir == "" {
+		t.Fatal("Got unexpected output from .New()")
+	} else if _, err := os.Stat(realBackupDir); os.IsNotExist(err) {
+		t.Fatalf("Cannot find backup dir after backup: %s", realBackupDir)
+	}
+
+	// this should fail because the backup path already exists
+	_, err = New(session, containerBackupDir)
+	if err == nil {
+		t.Fatal("Expected failure from .New() on second attempt")
+	}
+}
+
+// TODO: use a real Hot Backup instead of simulation
+func TestHotBackupDir(t *testing.T) {
+	hb := &HotBackup{backupDir: "/dev/null"}
+	if hb.Dir() != "/dev/null" {
+		t.Fatal("Unexpected output from .Dir()")
+	}
+}
+
+// TODO: use a real Hot Backup instead of simulation
+func TestHotBackupRemove(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", t.Name())
+	if err != nil {
+		t.Fatalf("Could not create temp dir: %v", err.Error())
+	}
+	defer os.RemoveAll(tempDir)
+
+	hb := &HotBackup{backupDir: tempDir}
+	err = hb.Remove()
+	if err != nil {
+		t.Fatalf("Failed to run .Remove(): %v", err.Error())
+	} else if _, err := os.Stat(tempDir); err == nil {
+		t.Fatal("Backup dir should not exist after .Remove()")
+	} else if !hb.removed {
+		t.Fatal("'removed' field should be true after .Remove()")
+	}
+}
+
+// TODO: use a real Hot Backup instead of simulation
+func TestHotBackupClose(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", t.Name())
+	if err != nil {
+		t.Fatalf("Could not create temp dir: %v", err.Error())
+	}
+	defer os.RemoveAll(tempDir)
+
+	hb := &HotBackup{backupDir: tempDir}
+	hb.Close()
+
+	if !hb.removed {
+		t.Fatal("'removed' field should be true after .Close()")
+	} else if hb.backupDir != "" {
+		t.Fatal("'backupDir' field should be empty after .Close()")
+	}
+}
