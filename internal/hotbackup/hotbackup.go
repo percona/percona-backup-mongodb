35570815ce6b70850a2b50dea6ee1060f692aac1
Tim Vaillancourt <tim@timvaillancourt.com>
2018-08-19 18:38:12 +0200

    Add hotbackup support

---
+++ internal/hotbackup/hotbackup.go
@@ -0,0 +1,61 @@
+package hotbackup
+
+import (
+	"errors"
+
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"github.com/percona/mongodb-backup/mdbstructs"
+)
+
+type HotBackup struct {
+	backupDir string
+	response  *mdbstructs.OkResponse
+	removed   bool
+}
+
+func isLocalhostSession(session *mgo.Session) bool {
+	servers := session.LiveServers()
+	if len(servers) != 1 {
+		return false
+	}
+	split := strings.Split(servers[0], ":")
+	return split[0] == "127.0.0.1" || split[0] == "localhost"
+}
+
+func New(session *mgo.Session, backupDir string) (*HotBackup, error) {
+	hb := Hotbackup{}
+	if !isLocalhostSession(session) {
+		return nil, errors.New("session must be direct session to localhost or 127.0.0.1")
+	}
+	err := session.Run(bson.D{{"createBackup", 1}, {"backupDir", backupDir}}, &hb.response)
+	if err != nil {
+		return nil, err
+	}
+	return hb, nil
+}
+
+func (hb *HotBackup) Response() *mdbstructs.OkResponse {
+	return hb.response
+}
+
+func (hb *HotBackup) Dir() string {
+	return hb.backupDir
+}
+
+func (hb *HotBackup) Remove() error {
+	if hb.removed {
+		return nil
+	}
+	err := os.RemoveAll(hb.backupDir)
+	if err != nil {
+		return err
+	}
+	hb.backupDir = ""
+	hb.removed = true
+	return nil
+}
+
+func (hb *HotBackup) Close() {
+	_ = hb.Remove()
+}
