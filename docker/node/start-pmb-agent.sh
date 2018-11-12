f2c5c6e8ac76f62ffa04d19fb4195ace5e586c11
Tim Vaillancourt <tim@timvaillancourt.com>
2018-11-12 20:02:27 +0100

    Update agent backup dir

--- docker/node/start-pmb-agent.sh
+++ docker/node/start-pmb-agent.sh
@@ -5,6 +5,10 @@ sleep 10
 
 # (re)start pmb-agent
 while true; do
-    /usr/local/bin/pmb-agent --mongodb-host=127.0.0.1 --mongodb-port=27017
+    pmb-agent \
+	    --backup-dir=/data/pmb \
+	    --mongodb-host=127.0.0.1 \
+	    --mongodb-port=27017
+    echo "Got exit code $? from pmb-agent, restarting"
     sleep 1
 done
