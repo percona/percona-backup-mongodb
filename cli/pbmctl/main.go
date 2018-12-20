1b52ab05567fa9677605af37ee7efc9e07dc96be
Tim Vaillancourt <tim@timvaillancourt.com>
2018-12-20 23:07:25 +0100

    Fix server help-field

--- cli/pbmctl/main.go
+++ cli/pbmctl/main.go
@@ -336,10 +336,10 @@ func processCliArgs(args []string) (string, *cliOptions, error) {
 		restoreSkipUsersAndRoles: restoreCmd.Flag("skip-users-and-roles", "Do not restore users and roles").Default("true").Bool(),
 	}
 
+	app.Flag("server-address", "Backup server address (host:port)").Default(defaultServerAddr).StringVar(&opts.ServerAddr)
+	app.Flag("server-compressor", "Backup server gRPC compression algorithm").Default(snappy.Name).EnumVar(&opts.ServerCompressor, grpcCompressors...)
 	app.Flag("tls", "Connection uses TLS if true, else plain TCP").Default("false").BoolVar(&opts.TLS)
 	app.Flag("tls-ca-file", "The file containning the CA root cert file").ExistingFileVar(&opts.TLSCAFile)
-	app.Flag("server-address", "The server address in the format of host:port").Default(defaultServerAddr).StringVar(&opts.ServerAddr)
-	app.Flag("server-compressor", "server gRPC compression algorithm").Default(snappy.Name).EnumVar(&opts.ServerCompressor, grpcCompressors...)
 
 	yamlOpts := &cliOptions{
 		ServerAddr: defaultServerAddr,
