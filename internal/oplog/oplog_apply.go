5cdf18336eae218f9b368c41029bf9d83cff02ed
Carlos Salguero <carlos.salguero@percona.com>
2019-07-19 08:24:03 -0300

    Oplog applier using bson.D

--- internal/oplog/oplog_apply.go
+++ internal/oplog/oplog_apply.go
@@ -3,7 +3,6 @@ package oplog
 import (
 	"fmt"
 	"io"
-	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -174,7 +173,7 @@ func (oa *Apply) Run() error {
 		    },
 		}
 		*/
-		dest := bson.M{}
+		dest := bson.D{}
 		buf, err := oa.bsonReader.ReadNext()
 		if err != nil {
 			if err == io.EOF {
@@ -184,28 +183,25 @@ func (oa *Apply) Run() error {
 			return merr
 		}
 
-		if err := bson.Unmarshal(buf, dest); err != nil {
+		if err := bson.Unmarshal(buf, &dest); err != nil {
 			return fmt.Errorf("cannot unmarshal oplog document: %s", err)
 		}
 
-		if ts, ok := dest["ts"].(bson.MongoTimestamp); ok {
+		destMap := dest.Map()
+
+		if ts, ok := destMap["ts"].(bson.MongoTimestamp); ok {
 			if oa.fcheck(ts, oa.stopAtTs) {
 				return nil
 			}
 		} else {
-			return fmt.Errorf("oplog document ts field is not bson.MongoTimestamp, it is %T", dest["ts"])
-		}
-
-		icmd, ok := dest["op"]
-		if !ok {
-			return fmt.Errorf("invalid oplog document. there is no command")
+			return fmt.Errorf("oplog document ts field is not bson.MongoTimestamp, it is %T", destMap["ts"])
 		}
 
 		if atomic.LoadUint32(&oa.debug) != 0 {
 			fmt.Printf("%#v\n", dest)
 		}
 
-		if ns, ok := dest["ns"]; ok {
+		if ns, ok := destMap["ns"]; ok {
 			if nss, ok := ns.(string); ok {
 				if skip(nss) {
 					continue
@@ -215,49 +211,25 @@ func (oa *Apply) Run() error {
 			}
 		}
 
-		if cmd, ok := icmd.(string); ok && cmd == "c" {
-			switch getOplogCmd(dest) {
-			case createCollectionOp:
-				err := processCreateCollection(oa.dbSession, buf)
-				if err != nil {
-					return errors.Wrapf(err, "cannot create collection from oplog cmd %+v", dest)
-				}
-				continue
-			case createIndexOp:
-				err := processCreateIndex(oa.dbSession, buf)
-				if err != nil {
-					return errors.Wrapf(err, "cannot create index from oplog cmd %+v", dest)
-				}
-				continue
-			case dropColOp:
-				err := processDropColCmd(oa.dbSession, buf)
-				if err != nil {
-					return errors.Wrapf(err, "cannot drop collection from oplog cmd %+v", dest)
-				}
-				continue
-			case dropIndexOp:
-				if err := processDropIndexCmd(oa.dbSession, buf); err != nil {
-					return errors.Wrapf(err, "cannot drop index from oplog cmd %+v", dest)
-				}
-				continue
-			default:
-				log.Errorf("unknown command in op: %#v", dest)
-			}
-		}
-
 		/* TODO: Fix me
 		If a collection was created by the oplog in the lines above, I still don't know how to
 		preserve the collection's UUID so, remove the UUIDs from all the Insert/Update/Delete
 		*/
-		if _, ok = dest["ui"]; ok {
-			delete(dest, "ui")
+		//if _, ok = dest["ui"]; ok {
+		//	delete(dest, "ui")
+		//}
+		for i, val := range dest {
+			if val.Name == "ui" {
+				dest = append(dest[:i], dest[i+1:]...)
+				break
+			}
 		}
 
 		result := bson.M{}
 		//TODO: Improve this. Instead of applying ops one by one (the array has one element)
 		// collect several documents. What happens if there are errors? Will it fail only in
 		// one operation or all operations would fail?
-		if err := oa.dbSession.Run(bson.M{"applyOps": []bson.M{dest}}, result); err != nil {
+		if err := oa.dbSession.Run(bson.M{"applyOps": []bson.D{dest}}, result); err != nil {
 			log.Errorf("cannot apply oplog: %s\n%+v\n", err, dest)
 			if !oa.ignoreErrors {
 				return errors.Wrap(err, "cannot apply oplog operation")
@@ -266,218 +238,6 @@ func (oa *Apply) Run() error {
 		}
 		atomic.AddInt64(&oa.docsCount, 1)
 	}
-}
-
-/*
-bson.M{
-    "op":   "c",
-    "ns":   "test.$cmd",
-    "wall": time.Time{
-        wall: 0x4b571c0,
-        ext:  63693701482,
-        loc:  (*time.Location)(nil),
-    },
-    "o": bson.M{
-        "capped":      bool(true),
-        "size":        int(1000192),
-        "autoIndexId": bool(false),
-        "create":      "testcol_03",
-    },
-    "ts": bson.MongoTimestamp(6692008652934479875),
-    "t":  int64(1),
-    "h":  int64(4522109498565533103),
-    "v":  int(2),
-}
-*/
-func processCreateCollection(sess *mgo.Session, buf []byte) error {
-	opdoc := createCollectionDoc{}
-	if err := bson.Unmarshal(buf, &opdoc); err != nil {
-		return errors.Wrap(err, "cannot unmarshal create collection command")
-	}
-
-	ns := strings.TrimSuffix(opdoc.NS, ".$cmd")
-
-	if ns == "" {
-		return fmt.Errorf("invalid namespace (empty)")
-	}
-
-	autoIndexID := !opdoc.O.AutoIndexID
-	if _, ok := opdoc.O.IDIndex.Key["_id"]; ok {
-		autoIndexID = true
-	}
-	info := &mgo.CollectionInfo{
-		DisableIdIndex: !autoIndexID,
-		ForceIdIndex:   opdoc.O.AutoIndexID,
-		Capped:         opdoc.O.Capped,
-		MaxBytes:       opdoc.O.Size,
-		MaxDocs:        opdoc.O.Max,
-	}
-
-	if err := sess.DB(ns).C(opdoc.O.Create).Create(info); err != nil {
-		return err
-	}
-	return nil
-}
-
-/*
-   "o": bson.M{
-       "key": bson.M{
-           "f1": int(1),
-           "f2": int(-1),
-       },
-       "name":          "f1_1_f2_-1",
-       "background":    bool(true),
-       "sparse":        bool(true),
-       "createIndexes": "testcol_00",
-       "v":             int(2),
-       "unique":        bool(true),
-   },
-*/
-func processCreateIndex(sess *mgo.Session, buf []byte) error {
-	opdoc := createIndexDoc{}
-	if err := bson.Unmarshal(buf, &opdoc); err != nil {
-		return errors.Wrap(err, "cannot unmarshal create collection command")
-	}
-
-	ns := strings.TrimSuffix(opdoc.NS, ".$cmd")
-
-	if ns == "" {
-		return fmt.Errorf("invalid namespace (empty)")
-	}
-
-	index := mgo.Index{
-		Key:              []string{},
-		Unique:           opdoc.O.Unique,
-		DropDups:         opdoc.O.DropDups,
-		Background:       opdoc.O.Background,
-		Sparse:           opdoc.O.Sparse,
-		PartialFilter:    opdoc.O.PartialFilter,
-		ExpireAfter:      opdoc.O.ExpireAfter,
-		Name:             opdoc.O.Name,
-		Min:              opdoc.O.Min,
-		Max:              opdoc.O.Max,
-		Minf:             opdoc.O.Minf,
-		Maxf:             opdoc.O.Maxf,
-		BucketSize:       opdoc.O.BucketSize,
-		Bits:             opdoc.O.Bits,
-		DefaultLanguage:  opdoc.O.DefaultLanguage,
-		LanguageOverride: opdoc.O.LanguageOverride,
-		Weights:          opdoc.O.Weights,
-		Collation:        opdoc.O.Collation,
-	}
-	for _, key := range opdoc.O.Key {
-		sign := ""
-		switch k := key.Value.(type) {
-		case int:
-			if k < 0 {
-				sign = "-"
-			}
-		case int8:
-			if k < 0 {
-				sign = "-"
-			}
-		case int16:
-			if k < 0 {
-				sign = "-"
-			}
-		case int32:
-			if k < 0 {
-				sign = "-"
-			}
-		case int64:
-			if k < 0 {
-				sign = "-"
-			}
-		case float32:
-			if k < 0 {
-				sign = "-"
-			}
-		case float64:
-			if k < 0 {
-				sign = "-"
-			}
-		}
-		if key.Name != "_fts" && key.Name != "_ftsx" {
-			index.Key = append(index.Key, sign+key.Name)
-		}
-	}
-
-	sess.ResetIndexCache()
-	err := sess.DB(ns).C(opdoc.O.CreateIndexes).EnsureIndex(index)
-	if err != nil {
-		return errors.Wrapf(err, "cannot create index %+v", index)
-	}
-	sess.ResetIndexCache()
-	return nil
-}
-
-func processDropColCmd(sess *mgo.Session, buf []byte) error {
-	opdoc := dropColDoc{}
-	if err := bson.Unmarshal(buf, &opdoc); err != nil {
-		return errors.Wrap(err, "cannot unmarshal drop collection command")
-	}
-
-	ns := strings.TrimSuffix(opdoc.NS, ".$cmd")
-
-	if ns == "" {
-		return fmt.Errorf("invalid namespace (empty)")
-	}
-	return sess.DB(ns).C(opdoc.O.Drop).DropCollection()
-}
-
-func processDropIndexCmd(sess *mgo.Session, buf []byte) error {
-	opdoc := dropIndexDoc{}
-	if err := bson.Unmarshal(buf, &opdoc); err != nil {
-		return errors.Wrap(err, "cannot unmarshal drop index command")
-	}
-
-	ns := strings.TrimSuffix(opdoc.NS, ".$cmd")
-
-	if ns == "" {
-		return fmt.Errorf("invalid namespace (empty)")
-	}
-	result := struct {
-		ErrMsg string
-		Ok     bool
-	}{}
-	sess.ResetIndexCache()
-	// Don't use mgo's DropIndex method because it will add a suffix
-	op := bson.D{{Name: "dropIndexes", Value: opdoc.O.DropIndexes}, {Name: "index", Value: opdoc.O.Index}}
-	sess.ResetIndexCache()
-	return sess.DB(ns).Run(op, &result)
-}
-
-func getOplogCmd(doc bson.M) int {
-	om, ok := doc["o"]
-	if !ok {
-		return invalidOp
-	}
-	o, ok := om.(bson.M)
-	if !ok {
-		return invalidOp
-	}
-
-	switch {
-	case getString(o, "create"):
-		return createCollectionOp
-	case getString(o, "createIndexes"):
-		return createIndexOp
-	case getString(o, "drop"):
-		return dropColOp
-	case getString(o, "dropIndexes"):
-		return dropIndexOp
-	}
-
-	return invalidOp
-}
-
-func getString(o bson.M, key string) bool {
-	if cmd, ok := o[key]; ok {
-		if _, ok := cmd.(string); ok {
-			return true
-		}
-	}
-	return false
 }
 
 func (oa *Apply) Count() int64 {
