3e00ab00e3c31fd7f5be5a4789a2fe8912380009
Andrew Pogrebnoy <absourd.noise@gmail.com>
2020-03-10 22:28:11 +0200

    Improve k8s' docker start script

--- docker/start-agent.sh
+++ docker/start-agent.sh
@@ -3,16 +3,17 @@
 export PBM_MONGODB_URI="mongodb://${PBM_AGENT_MONGODB_USERNAME}:${PBM_AGENT_MONGODB_PASSWORD}@localhost:${PBM_MONGODB_PORT}/?replicaSet=${PBM_MONGODB_REPLSET}"
 
 OUT="$(mktemp)"
+PSMDB_RS_SIZE=${PSMDB_RS_SIZE:-1}
 timeout=5
 
 for i in {1..10}; do
-    mongo ${PBM_MONGODB_URI} --eval="db.isMaster().ok" --quiet | tee "$OUT"
+    mongo ${PBM_MONGODB_URI} --eval="(db.isMaster().hosts).length" --quiet | tee "$OUT"
     exit_status=$?
-    ok=$(egrep "^(0|1)$" "$OUT")
-    if [[ "${exit_status}" != 0 ]] || [[ $ok != 1 ]]; then
-        sleep "$((timeout * i))"
-    else
+    rs_size=$(egrep "^(\d+)$" "$OUT")
+    if [[ "${exit_status}" == 0 ]] && [[ $rs_size == $PSMDB_RS_SIZE || $rs_size > 1 ]] ; then
         break
+    else
+        sleep "$((timeout * i))"
     fi
 done
 
