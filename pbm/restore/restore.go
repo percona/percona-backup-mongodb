8b90e5ae738dc2d85f9e2d7b8c52cc967eff8b9c
Andrew Pogrebnoy <absourd.noise@gmail.com>
2021-04-09 19:06:40 +0300

    PBM-661: improve logging and rollback restore

--- pbm/restore/restore.go
+++ pbm/restore/restore.go
@@ -522,7 +522,6 @@ func (r *Restore) RunSnapshot() (err error) {
 	}
 	defer oplogReader.Close()
 
-	r.oplog.SetEdge(r.bcp.LastWriteTS)
 	lts, err := r.oplog.Apply(oplogReader)
 	if err != nil {
 		return errors.Wrap(err, "oplog apply")
