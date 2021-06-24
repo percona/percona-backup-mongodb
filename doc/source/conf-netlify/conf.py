66f16b21cd85596c715b8cc1fb7021aaad8969ee
Anastasia Alexandrova <anastasia.alexandrova@percona.com>
2021-06-24 17:58:19 +0300

    PBM-665 Rebased on main, added plantuml to dependencies list

--- doc/source/conf-netlify/conf.py
+++ doc/source/conf-netlify/conf.py
@@ -5,8 +5,9 @@ import os
 sys.path.append(os.path.abspath("../"))
 from conf import *
 extensions.append('sphinx_gitstamp')
-#extensions.append('sphinx_copybutton')
+extensions.append('sphinx_copybutton')
 html_theme = 'sphinx_material'
+html_sidebars['**']=['globaltoc.html', 'searchbox.html', 'localtoc.html', 'logo-text.html']
 html_theme_options = {
     'base_url': 'http://bashtage.github.io/sphinx-material/',
     'repo_url': 'https://github.com/percona/percona-backup-mongodb',
@@ -18,4 +19,6 @@ html_logo = '../_images/percona-logo.svg'
 html_favicon = '../_images/percona_favicon.ico'
 pygments_style = 'emacs'
 gitstamp_fmt = "%b %d, %Y"
+copybutton_prompt_text = '$'
+plantuml = 'java -jar ../../bin/plantuml.jar'
 #html_last_updated_fmt = ''
