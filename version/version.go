e2b6047ea45692f0ab61d08f9dc775f6b4c7aefb
Andrew Pogrebnoy <absourd.noise@gmail.com>
2022-01-04 13:06:32 +0200

    Fix vesion typo

--- version/version.go
+++ version/version.go
@@ -9,7 +9,7 @@ import (
 )
 
 // current PBM version
-const version = "2.0.0-aplha"
+const version = "2.0.0-alpha"
 
 // !!! should be sorted in the ascending order
 var breakingChangesV = []string{
