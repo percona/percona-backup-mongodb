ded98ce9e015e9925871d3a51a9f5575a134aaa1
Andrew Pogrebnoy <absourd.noise@gmail.com>
2022-09-15 17:05:09 +0300

    PBM-941: add sidecar mode for pbm-agent
    Add an entry point app that starts pbm-agent in k8s. In case env
    `PBM_AGENT_SIDECAR` is set to true, it will restart the agent should it
    exits (despite withan error or not) after the `PBM_AGENT_SIDECAR_SLEEP`
    timeout in sec (default 1)
    
    https://jira.percona.com/browse/PBM-941

--- cmd/pbm-agent-entrypoint/main.go
+++ cmd/pbm-agent-entrypoint/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"log"
 	"os"
 	"os/exec"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
 )
 
 const (
@@ -13,11 +18,77 @@ const (
 )
 
 func main() {
-	_, isSidecar := os.LookupEnv(envSidecar)
+	var procID int
+
+	l := log.New(os.Stderr, "[entrypoint] ", log.LstdFlags|log.Lmsgprefix)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		s := <-sig
+		signal.Stop(sig)
+
+		l.Printf("got %s, shutting down", s)
+		if procID != 0 {
+			err := syscall.Kill(procID, syscall.SIGTERM)
+			l.Printf("kill `%s` (%d): %v", agentCmd, procID, err)
+		}
+		os.Exit(0)
+	}()
+
+	var (
+		isSidecar bool
+		sleepSec  = 1
+
+		err error
+	)
+	if isSideStr, ok := os.LookupEnv(envSidecar); ok {
+		isSidecar, err = strconv.ParseBool(isSideStr)
+		if err != nil {
+			l.Printf("error: parsing %s value: %v", envSidecar, err)
+		}
+	}
+	if sleepStr, ok := os.LookupEnv(envSidecarSleepSec); ok {
+		sec, err := strconv.Atoi(sleepStr)
+		if err != nil {
+			l.Printf("error: parsing %s value: %v", envSidecarSleepSec, err)
+		} else {
+			sleepSec = sec
+		}
+	}
+
+	var exitCode int
 	for {
-		cmd := exec.Command(agentCmd, os.Args...)
+		cmd := exec.Command(agentCmd, os.Args[1:]...)
 		cmd.Stderr = os.Stderr
 		cmd.Stdin = os.Stdin
-		cmd.Run()
+
+		exitCode = 1
+
+		l.Printf("starting `%s`", agentCmd)
+		err := cmd.Start()
+		if err != nil {
+			l.Printf("error: starting `%s`: %v", agentCmd, err)
+			if !isSidecar {
+				os.Exit(exitCode)
+			}
+		}
+
+		procID = cmd.Process.Pid
+
+		err = cmd.Wait()
+		if err != nil {
+			if exErr, ok := err.(*exec.ExitError); ok {
+				exitCode = exErr.ExitCode()
+			}
+		}
+		if !isSidecar {
+			os.Exit(exitCode)
+		}
+
+		l.Printf("`%s` exited with code %d", agentCmd, exitCode)
+		l.Printf("restart in %d sec", sleepSec)
+		time.Sleep(time.Second * time.Duration(sleepSec))
 	}
 }
