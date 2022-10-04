566a61c35d1c9e449ba255d4cd22121c91572646
Andrew Pogrebnoy <absourd.noise@gmail.com>
2022-10-04 17:53:01 +0300

    PBM-949: don't wait for hidden and arbiter nodes for phys restore

--- pbm/restore/physical.go
+++ pbm/restore/physical.go
@@ -989,7 +989,9 @@ func (r *PhysRestore) init(name string, opid pbm.OPID, l *log.Event) (err error)
 	r.syncPathCluster = fmt.Sprintf("%s/%s/cluster", pbm.PhysRestoresDir, r.name)
 	r.syncPathPeers = make(map[string]struct{})
 	for _, m := range r.rsConf.Members {
-		r.syncPathPeers[fmt.Sprintf("%s/%s/rs.%s/node.%s", pbm.PhysRestoresDir, r.name, r.rsConf.ID, m.Host)] = struct{}{}
+		if !m.ArbiterOnly && !m.Hidden {
+			r.syncPathPeers[fmt.Sprintf("%s/%s/rs.%s/node.%s", pbm.PhysRestoresDir, r.name, r.rsConf.ID, m.Host)] = struct{}{}
+		}
 	}
 
 	err = r.hb()
