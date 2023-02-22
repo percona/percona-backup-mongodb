e300a1c746f01d81de7c84b74ca272e5db00c2b1
Dmytro Zghoba <dmytro.zghoba@percona.com>
2023-02-22 17:32:21 +0200

    [PBM-1007] add support for 4.2 and 4.4

--- pbm/sel/sel.go
+++ pbm/sel/sel.go
@@ -1,7 +1,11 @@
 package sel
 
 import (
+	"encoding/hex"
 	"strings"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/percona/percona-backup-mongodb/pbm/archive"
 )
@@ -40,4 +44,64 @@ func MakeSelectedPred(nss []string) archive.NSFilterFn {
 		db, coll, ok := strings.Cut(ns, ".")
 		return (m[""] != nil || m[db][""]) || (ok && m[db][coll])
 	}
+}
+
+type ChunkSelector interface {
+	Add(bson.Raw)
+	Selected(bson.Raw) bool
+
+	BuildFilter() bson.D
+}
+
+type nsChunkMap map[string]struct{}
+
+func NewNSChunkSelector() ChunkSelector {
+	return make(nsChunkMap)
+}
+
+func (s nsChunkMap) Add(d bson.Raw) {
+	ns := d.Lookup("_id").StringValue()
+	s[ns] = struct{}{}
+}
+
+func (s nsChunkMap) Selected(d bson.Raw) bool {
+	ns := d.Lookup("ns").StringValue()
+	_, ok := s[ns]
+	return ok
+}
+
+func (s nsChunkMap) BuildFilter() bson.D {
+	nss := make([]string, 0, len(s))
+	for ns := range s {
+		nss = append(nss, ns)
+	}
+
+	return bson.D{{"ns", bson.M{"$in": nss}}}
+}
+
+type uuidChunkMap map[string]struct{}
+
+func NewUUIDChunkSelector() ChunkSelector {
+	return make(uuidChunkMap)
+}
+
+func (s uuidChunkMap) Add(d bson.Raw) {
+	_, data := d.Lookup("uuid").Binary()
+	s[hex.EncodeToString(data)] = struct{}{}
+}
+
+func (s uuidChunkMap) Selected(d bson.Raw) bool {
+	_, data := d.Lookup("uuid").Binary()
+	_, ok := s[hex.EncodeToString(data)]
+	return ok
+}
+
+func (s uuidChunkMap) BuildFilter() bson.D {
+	uuids := make([]primitive.Binary, 0, len(s))
+	for ns := range s {
+		data, _ := hex.DecodeString(ns)
+		uuids = append(uuids, primitive.Binary{0x4, data})
+	}
+
+	return bson.D{{"uuid", bson.M{"$in": uuids}}}
 }
