9b22da956ff4425137dc79ee71ad77cf2524b27b
Dmytro Zghoba <dmytro.zghoba@percona.com>
2023-07-24 11:03:11 +0300

    [PBM-1155] use new thisBackupName on each retries

--- pbm/pbm.go
+++ pbm/pbm.go
@@ -549,6 +549,10 @@ type BackupReplset struct {
 	Error            string              `bson:"error,omitempty" json:"error,omitempty"`
 	Conditions       []Condition         `bson:"conditions" json:"conditions"`
 	MongodOpts       *MongodOpts         `bson:"mongod_opts,omitempty" json:"mongod_opts,omitempty"`
+
+	// CustomThisID is customized thisBackupName value for $backupCursor (in WT: "this_id").
+	// If it is not set (empty), the default value was used.
+	CustomThisID string `bson:"this_id,omitempty" json:"this_id,omitempty"`
 }
 
 type File struct {
