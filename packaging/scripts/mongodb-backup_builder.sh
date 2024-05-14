be273eee454fbbb2bd269bb46c24078b35d98894
Sandra Romanchenko <53295797+sandraromanchenko@users.noreply.github.com>
2024-05-14 18:40:45 +0300

    PBM. Increase go version to fix CVE-2024-24788 (#945)

--- packaging/scripts/mongodb-backup_builder.sh
+++ packaging/scripts/mongodb-backup_builder.sh
@@ -141,7 +141,7 @@ install_golang() {
     elif [ x"$ARCH" = "xaarch64" ]; then
         GO_ARCH="arm64"
     fi
-    wget https://go.dev/dl/go1.22.2.linux-${GO_ARCH}.tar.gz -O /tmp/go1.22.tar.gz
+    wget https://go.dev/dl/go1.22.3.linux-${GO_ARCH}.tar.gz -O /tmp/go1.22.tar.gz
     tar --transform=s,go,go1.22, -zxf /tmp/go1.22.tar.gz
     rm -rf /usr/local/go*
     mv go1.22 /usr/local/
