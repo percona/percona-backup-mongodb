af46ae9ae08e29b09dd331bec7c631425ec411d7
Dmytro Zghoba <dmytro.zghoba@percona.com>
2024-06-13 16:55:48 +0200

    fix external restore

--- cmd/pbm-agent/restore.go
+++ cmd/pbm-agent/restore.go
@@ -98,6 +98,7 @@ func (a *Agent) Restore(ctx context.Context, r *ctrl.RestoreCmd, opid ctrl.OPID,
 			return
 		}
 		bcpType = bcp.Type
+		r.BackupName = bcp.Name
 	}
 
 	l.Info("recovery started")
@@ -130,7 +131,6 @@ func (a *Agent) Restore(ctx context.Context, r *ctrl.RestoreCmd, opid ctrl.OPID,
 			return
 		}
 
-		r.BackupName = bcp.Name
 		err = rstr.Snapshot(ctx, r, r.OplogTS, opid, l, a.closeCMD, a.HbPause)
 	}
 	if err != nil {
