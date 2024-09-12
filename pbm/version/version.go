c8fd6bed8fe203b4648be95553ecdb5823d6587a
Dmytro Zghoba <dmytro.zghoba@gmail.com>
2024-09-12 14:41:11 +0200

    PBM-1389: allow to run on MongoDB 8.0

--- pbm/version/version.go
+++ pbm/version/version.go
@@ -212,11 +212,11 @@ type FeatureSupport MongoVersion
 func (f FeatureSupport) PBMSupport() error {
 	v := MongoVersion(f)
 
-	if (v.Version[0] >= 5 && v.Version[0] <= 7) && v.Version[1] == 0 {
+	if (v.Version[0] >= 5 && v.Version[0] <= 8) && v.Version[1] == 0 {
 		return nil
 	}
 
-	return errors.New("Unsupported MongoDB version. PBM works with v5.0, v6.0, v7.0")
+	return errors.New("Unsupported MongoDB version. PBM works with v5.0, v6.0, v7.0, v8.0")
 }
 
 func (f FeatureSupport) FullPhysicalBackup() bool {
