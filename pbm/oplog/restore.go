f6542f579ca62d61586e38432667509090bf8fef
Boris Ilijic <boris.ilijic@percona.com>
2024-09-17 12:24:11 +0200

    Fix typo

--- pbm/oplog/restore.go
+++ pbm/oplog/restore.go
@@ -325,7 +325,7 @@ func (o *OplogRestore) handleOp(oe db.Oplog) error {
 	if o.cnamespase != oe.Namespace {
 		o.preserveUUID = o.preserveUUIDopt
 
-		// if this is a create operation, the namesape would be
+		// if this is a create operation, the namespace would be
 		// inside the object to create
 		if oe.Operation == "c" {
 			if len(oe.Object) == 0 {
