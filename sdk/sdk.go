4f2bee8622278dbd6bb17bfdbba686b35ac6f8e6
Boris Ilijic <boris.ilijic@percona.com>
2024-10-09 12:15:28 +0200

    Inject node name from sdk
    ... for the purpose of storage creation and endpoint url mapping feature

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -2,7 +2,6 @@ package sdk
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -13,10 +12,12 @@ import (
 	"github.com/percona/percona-backup-mongodb/pbm/connect"
 	"github.com/percona/percona-backup-mongodb/pbm/ctrl"
 	"github.com/percona/percona-backup-mongodb/pbm/defs"
+	"github.com/percona/percona-backup-mongodb/pbm/errors"
 	"github.com/percona/percona-backup-mongodb/pbm/lock"
 	"github.com/percona/percona-backup-mongodb/pbm/log"
 	"github.com/percona/percona-backup-mongodb/pbm/oplog"
 	"github.com/percona/percona-backup-mongodb/pbm/restore"
+	"github.com/percona/percona-backup-mongodb/pbm/topo"
 )
 
 var (
@@ -137,7 +138,12 @@ func NewClient(ctx context.Context, uri string) (*Client, error) {
 		return nil, err
 	}
 
-	return &Client{conn: conn}, nil
+	inf, err := topo.GetNodeInfo(ctx, conn.MongoClient())
+	if err != nil {
+		return nil, errors.Wrap(err, "get node info")
+	}
+
+	return &Client{conn: conn, node: inf.Me}, nil
 }
 
 func WaitForAddProfile(ctx context.Context, client *Client, cid CommandID) error {
