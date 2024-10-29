bd2e3a39e05f4883d03c5809fbd192b8f5bcb678
Boris Ilijic <boris.ilijic@percona.com>
2024-10-29 11:12:19 +0100

    Disable oplog apply when cloning collection

--- pbm/restore/logical.go
+++ pbm/restore/logical.go
@@ -256,9 +256,15 @@ func (r *Restore) Snapshot(
 		oplogOption.nss = []string{"config.databases"}
 		oplogOption.filter = newConfigsvrOpFilter(nss)
 	}
-	err = r.applyOplog(ctx, oplogRanges, oplogOption)
-	if err != nil {
-		return err
+	if cloneNS.IsSpecified() {
+		// oplog doesn't need to be applied when cloning ns
+		// this restriction will be removed during PBM-1422
+		l.Debug("applying oplog is skipped when cloning collection")
+	} else {
+		err = r.applyOplog(ctx, oplogRanges, oplogOption)
+		if err != nil {
+			return err
+		}
 	}
 
 	err = r.restoreIndexes(ctx, oplogOption.nss, cloneNS)
