6232be53ff748924abb14c831cf075cc91380298
EvgeniyPatlan <evgeniy.patlan@percona.com>
2024-12-05 16:11:16 +0200

    Add AL support

--- packaging/scripts/mongodb-backup_builder.sh
+++ packaging/scripts/mongodb-backup_builder.sh
@@ -127,6 +127,11 @@ get_system() {
         ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
         OS_NAME="el$RHEL"
         OS="rpm"
+    elif [ -f /etc/amazon-linux-release ]; then
+        RHEL=$(rpm --eval %amzn)
+        ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
+        OS_NAME="amzn$RHEL"
+        OS="rpm"
     else
         ARCH=$(uname -m)
         OS_NAME="$(lsb_release -sc)"
@@ -141,7 +146,11 @@ install_golang() {
     elif [ x"$ARCH" = "xaarch64" ]; then
         GO_ARCH="arm64"
     fi
-    wget https://go.dev/dl/go1.22.8.linux-${GO_ARCH}.tar.gz -O /tmp/go1.22.tar.gz
+    for i in {1..3}; do
+        wget https://go.dev/dl/go1.22.8.linux-${GO_ARCH}.tar.gz -O /tmp/go1.22.tar.gz && break
+        echo "Failed to download GOLang, retrying in 10 seconds..."
+        sleep 10
+    done
     tar --transform=s,go,go1.22, -zxf /tmp/go1.22.tar.gz
     rm -rf /usr/local/go*
     mv go1.22 /usr/local/
@@ -160,7 +169,6 @@ install_deps() {
     CURPLACE=$(pwd)
 
     if [ "x$OS" = "xrpm" ]; then
-        RHEL=$(rpm --eval %rhel)
         yum clean all
         yum -y install epel-release git wget
         yum -y install rpm-build make rpmlint rpmdevtools golang krb5-devel
@@ -277,9 +285,6 @@ build_rpm() {
     rm -fr rpmbuild
     mkdir -vp rpmbuild/{SOURCES,SPECS,BUILD,SRPMS,RPMS}
     cp $SRC_RPM rpmbuild/SRPMS/
-
-    RHEL=$(rpm --eval %rhel)
-    ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
 
     echo "RHEL=${RHEL}" >>percona-backup-mongodb.properties
     echo "ARCH=${ARCH}" >>percona-backup-mongodb.properties
