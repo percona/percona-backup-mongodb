dfa699f113a6bbbf9f59f7426ba6049c70b252b8
Jacob Vecera <jakub.vecera@ext.percona.com>
2025-02-05 14:41:04 +0100

    remove gcs tweaks

--- pbm/storage/s3/s3.go
+++ pbm/storage/s3/s3.go
@@ -9,7 +9,6 @@ import (
 	"io"
 	"maps"
 	"net/http"
-	"net/url"
 	"os"
 	"path"
 	"reflect"
@@ -19,7 +18,6 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/retry"
-	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
@@ -609,13 +607,6 @@ func (s *S3) s3client() (*s3.Client, error) {
 		cfg.Credentials = aws.NewCredentialsCache(webRole)
 	}
 
-	if strings.Contains(s.opts.EndpointURL, "storage.googleapis.com") {
-		cfg.RequestChecksumCalculation = aws.RequestChecksumCalculationWhenRequired
-		cfg.HTTPClient = &http.Client{
-			Transport: &RecalculateV4Signature{http.DefaultTransport, v4.NewSigner(), cfg},
-		}
-	}
-
 	return s3.NewFromConfig(cfg, func(o *s3.Options) {
 		o.UsePathStyle = *s.opts.ForcePathStyle
 	}), nil
@@ -702,37 +693,4 @@ func toClientLogMode(levels string) aws.ClientLogMode {
 	}
 
 	return mode
-}
-
-type RecalculateV4Signature struct {
-	next   http.RoundTripper
-	signer *v4.Signer
-	cfg    aws.Config
-}
-
-func (lt *RecalculateV4Signature) RoundTrip(req *http.Request) (*http.Response, error) {
-	val := req.Header.Get("Accept-Encoding")
-
-	// delete the header so the header doesn't account for in the signature
-	req.Header.Del("Accept-Encoding")
-
-	// sign with the same date
-	timeString := req.Header.Get("X-Amz-Date")
-	timeDate, _ := time.Parse("20060102T150405Z", timeString)
-
-	creds, _ := lt.cfg.Credentials.Retrieve(req.Context())
-
-	u := url.URL{Path: req.URL.Path}
-	req.URL.RawPath = u.EscapedPath()
-
-	err := lt.signer.SignHTTP(req.Context(), creds, req, v4.GetPayloadHash(req.Context()), "s3", lt.cfg.Region, timeDate)
-	if err != nil {
-		return nil, err
-	}
-
-	// Reset Accept-Encoding
-	req.Header.Set("Accept-Encoding", val)
-
-	// follows up the original round tripper
-	return lt.next.RoundTrip(req)
 }
