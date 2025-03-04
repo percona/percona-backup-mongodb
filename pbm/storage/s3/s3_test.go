86763ff71e1636bcf0562738945431fdd5d704e3
Jacob Vecera <jakub.vecera@ext.percona.com>
2025-03-04 21:39:18 +0100

    update s3 coverage

--- pbm/storage/s3/s3_test.go
+++ pbm/storage/s3/s3_test.go
@@ -2,9 +2,11 @@ package s3
 
 import (
 	"context"
+	"github.com/percona/percona-backup-mongodb/pbm/errors"
 	"io"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -64,6 +66,7 @@ func TestS3(t *testing.T) {
 			AccessKeyID:     "minioadmin",
 			SecretAccessKey: "minioadmin",
 		},
+		Retryer: &Retryer{},
 	}
 
 	stg, err := New(opts, "node", nil)
@@ -208,4 +211,87 @@ func TestS3(t *testing.T) {
 			t.Errorf("expected content %s, got %s", content, string(data))
 		}
 	})
+}
+
+func TestConfig(t *testing.T) {
+	opts := &Config{
+		Bucket: "bucketName",
+		Prefix: "prefix",
+		Credentials: Credentials{
+			AccessKeyID:     "minioadmin",
+			SecretAccessKey: "minioadmin",
+		},
+		Retryer: &Retryer{
+			NumMaxRetries: 1,
+		},
+		ServerSideEncryption: &AWSsse{},
+	}
+
+	t.Run("Clone", func(t *testing.T) {
+		opts.ForcePathStyle = aws.Bool(true)
+		clone := opts.Clone()
+		if clone == opts {
+			t.Error("expected clone to be a different pointer")
+		}
+
+		if !opts.Equal(clone) {
+			t.Error("expected clone to be equal")
+		}
+
+		opts.Bucket = "updatedName"
+		if opts.Equal(clone) {
+			t.Error("expected clone to be unchanged when updating original")
+		}
+	})
+
+	t.Run("Equal fails", func(t *testing.T) {
+		if opts.Equal(nil) {
+			t.Error("expected not to be equal other nil")
+		}
+
+		clone := opts.Clone()
+		clone.Prefix = "updatedPrefix"
+		if opts.Equal(clone) {
+			t.Error("expected not to be equal when updating prefix")
+		}
+
+		clone = opts.Clone()
+		clone.Region = "updatedRegion"
+		if opts.Equal(clone) {
+			t.Error("expected not to be equal when updating region")
+		}
+
+		clone = opts.Clone()
+		clone.EndpointURL = "EndpointURL"
+		if opts.Equal(clone) {
+			t.Error("expected not to be equal when updating EndpointURL")
+		}
+
+		clone = opts.Clone()
+		clone.EndpointURLMap = map[string]string{"foo": "bar"}
+		if opts.Equal(clone) {
+			t.Error("expected not to be equal when updating EndpointURLMap")
+		}
+
+		clone = opts.Clone()
+		clone.Credentials.AccessKeyID = "updating"
+		if opts.Equal(clone) {
+			t.Error("expected not to be equal when updating credentials")
+		}
+	})
+}
+
+func TestRetryer(t *testing.T) {
+	minBackoff := 100 * time.Millisecond
+	retryer := NewCustomRetryer(2, minBackoff, 1000*time.Millisecond)
+
+	delay, _ := retryer.RetryDelay(3, errors.New("error"))
+
+	if delay < minBackoff {
+		t.Errorf("Expected delay to be at least %s, but got %s", minBackoff, delay)
+	}
+}
+
+func TestSaveWithSSE(t *testing.T) {
+
 }
