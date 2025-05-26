855732449af058a77e5ff8a8a671c4f2e12be60a
Jacob Vecera <jakub.vecera@ext.percona.com>
2025-05-26 13:45:34 +0200

    update variables

--- pbm/backup/slicer.go
+++ pbm/backup/slicer.go
@@ -29,12 +29,8 @@ func startOplogSlicer(
 	upload uploadChunkFunc,
 ) stopSlicerFunc {
 	l := log.LogEventFromContext(ctx)
-	var (
-		uploaded   int64
-		err        error
-		majority   bool
-		currOpTime primitive.Timestamp
-	)
+	var uploaded int64
+	var err error
 
 	stopSignalC := make(chan struct{})
 	stoppedC := make(chan struct{})
@@ -59,17 +55,17 @@ func startOplogSlicer(
 				return
 			}
 
-			majority, err = topo.IsWriteMajorityRequested(ctx, m, writeConcern)
-			if err != nil {
-				l.Error("failed to inspect requested majority: %v", err)
+			majority, e := topo.IsWriteMajorityRequested(ctx, m, writeConcern)
+			if e != nil {
+				l.Error("failed to inspect requested majority: %v", e)
 			}
-			currOpTime, err = topo.GetLastWrite(ctx, m, majority)
-			if err != nil {
-				if errors.Is(err, context.Canceled) {
+			currOpTime, e := topo.GetLastWrite(ctx, m, majority)
+			if e != nil {
+				if errors.Is(e, context.Canceled) {
 					return
 				}
 
-				l.Error("failed to get last write: %v", err)
+				l.Error("failed to get last write: %v", e)
 				continue
 			}
 			if !currOpTime.After(startOpTime) {
