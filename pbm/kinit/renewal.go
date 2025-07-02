11278b1687d773ef570cf19fe758df1db84528e8
veceraj <jakub.vecera@ext.percona.com>
2025-07-02 07:14:24 +0200

    update renewal

--- pbm/kinit/renewal.go
+++ pbm/kinit/renewal.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/url"
 	"os/exec"
+	"strings"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -12,48 +13,49 @@ import (
 	"github.com/percona/percona-backup-mongodb/pbm/log"
 )
 
-type RenewalManager struct {
+// Renewal periodically refreshes a Kerberos TGT using the `kinit` command-line tool.
+type Renewal struct {
 	Keytab    string
 	Principal string
 	Interval  time.Duration
 }
 
-func New(mongoURI, keytab string, interval time.Duration) (*RenewalManager, error) {
+func New(mongoURI, keytab string, interval time.Duration) (*Renewal, error) {
 	if _, err := exec.LookPath("kinit"); err != nil {
 		return nil, errors.New("kinit not found in PATH")
 	}
 
-	principal, err := principalFromMongoUri(mongoURI)
+	principal, err := principalFromMongoURI(mongoURI)
 	if err != nil {
 		return nil, errors.Wrap(err, "parsing principal")
 	}
 
-	return &RenewalManager{
+	return &Renewal{
 		Keytab:    keytab,
 		Principal: principal,
 		Interval:  interval,
 	}, nil
 }
 
-func (rm RenewalManager) Start(ctx context.Context) {
+// Start performs an immediate renewal and then continues to renew the ticket every Interval.
+func (r Renewal) Start(ctx context.Context) {
 	l := log.FromContext(ctx).NewEvent("", "", "", primitive.Timestamp{})
-	
-	if err := rm.renew(ctx); err != nil {
+
+	if err := r.renew(ctx); err != nil {
 		l.Error("Kerberos ticket renewal failed: %v", err)
-		// TODO: determine if it should exit
 	} else {
 		l.Info("Kerberos ticket obtained successfully")
 	}
 
-	tmr := time.NewTimer(rm.Interval)
+	tkr := time.NewTicker(r.Interval)
 	go func() {
-		defer tmr.Stop()
+		defer tkr.Stop()
 		for {
 			select {
 			case <-ctx.Done():
 				return
-			case <-tmr.C:
-				if err := rm.renew(ctx); err != nil {
+			case <-tkr.C:
+				if err := r.renew(ctx); err != nil {
 					l.Error("Kerberos ticket renewal failed: %v", err)
 				} else {
 					l.Info("Kerberos ticket obtained successfully")
@@ -63,21 +65,22 @@ func (rm RenewalManager) Start(ctx context.Context) {
 	}()
 }
 
-func (rm RenewalManager) renew(ctx context.Context) error {
+func (r Renewal) renew(ctx context.Context) error {
 	args := []string{"-k"}
-	if rm.Keytab != "" {
-		args = append(args, "-t", rm.Keytab)
+	if r.Keytab != "" {
+		args = append(args, "-t", r.Keytab)
 	}
-	args = append(args, rm.Principal)
+	args = append(args, r.Principal)
 
 	cmd := exec.CommandContext(ctx, "kinit", args...)
-	if _, err := cmd.CombinedOutput(); err != nil {
-		return errors.Wrap(err, "kinit failed")
+	if out, err := cmd.CombinedOutput(); err != nil {
+		msg := strings.TrimSpace(string(out))
+		return errors.New(msg)
 	}
 	return nil
 }
 
-func principalFromMongoUri(uri string) (string, error) {
+func principalFromMongoURI(uri string) (string, error) {
 	parsed, err := url.Parse(uri)
 	if err != nil {
 		return "", errors.Wrap(err, "cannot parse URI")
